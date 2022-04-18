f6fb40d0e89d868ef86c44fe9dbf66a4868da40a
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-04-18 14:16:13 +0700

    Styling component in React

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,21 @@
 import React from "react";
 import Header from "./Header";
 import List from "./List";
+import "../style/App.css";
 
-class App extends React.Component {
-    state = {
-        name: "12-04-2022",
-        text: "",
-    }
+const App = () => {
 
-    click = () => {
-        this.setState({
-            name: this.state.text,
-            text: ""
-        })
-    }
-
-    change = e => {
-        this.setState({
-            text: e.target.value
-        })
-    }
-    render() {
-        return (
-
-        <div>
+    return (
+        <div className="container">
             <Header/>
-                Hello World
-                <input type="text" value={this.state.text} onChange={this.change}/>
-                <button onClick={this.click}> click </button>
-                <p>{this.state.name }</p>
-                <List name="Props name list" date={this.state.date}>this children form List </List>
+            <h1 style={mystyle}>Hello World</h1>
+            <List/>
         </div>
-        )
-    }
+    )
 }
-
-// untuk pembuat component di React bisa menggunakan class atau function
-
-// const App = () => {
-// return (
-//     <div>
-//         Hello World
-//         </div>
-//     )
-// }
+const mystyle = {
+    color: "blue",
+    fontSize: "20px"
+}
 
 export default App;
